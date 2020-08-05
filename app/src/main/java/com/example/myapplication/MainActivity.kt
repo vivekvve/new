8951a93341244e40b27e12a90d7a458a0a6de113
acer <vivekchiliveri1289@gmail.com>
2020-08-05 02:10:23 -0700

    section two

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main
+        setContentView(R.layout.activity_main)
+
+            btnshowtoast.setOnClickListener {
+
+                Log.i("MainActivity", "Button was clicked !")
+
+                Toast.makeText(this,   "Button was clicked !", Toast.LENGTH_SHORT).show()
+            }
+
     }
 
 }
